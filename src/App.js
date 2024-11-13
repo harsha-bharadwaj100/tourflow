dfa76cdaa3e61dfa9b87d4932e46e503228f9896
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2024-11-13 20:27:44 +0530

    siking

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import Home from "./Components/Home/Home";
-import Main from "./Components/Main/Main";
 import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Main from "./Components/Main/Main"; // Add import statement for Main component
+import Footer from "./Components/Footer/Footer"; // Add import statement for Footer component
 
 const App = () => {
   return (
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
-import './navbar.css'
+import React, { useState } from 'react'; 
+import './navbar.css';
+import { MdOutlineTravelExplore } from 'react-icons/md';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { TbGridDots } from 'react-icons/tb';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = useState(false);
 
-export default Navbar
+  const toggleNav = () => {
+    setActive(!active);
+  };
+
+  return (
+    <section className='navBarSection'>
+      <header className="header flex">
+        <div className="logoDiv">
+          <a href="#" className="logo flex">
+            <h1><MdOutlineTravelExplore className="icon" /> Travel.</h1>
+          </a>
+        </div>
+
+        <div className={`navBar ${active ? 'activeNavBar' : ''}`}>
+          <div className="closeNavbar" onClick={toggleNav}>
+            <AiFillCloseCircle className="icon" />
+          </div>
+          <ul className="navLists flex">
+            <li className="navItem">
+              <a href="#" className="navlink">Home</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navlink">Packages</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navlink">Shop</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navlink">About</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navlink">Pages</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navlink">News</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navlink">Contact</a>
+            </li>
+            <button className='btn'>
+              <a href="#">Book Now</a>
+            </button>
+          </ul>
+        </div>
+
+        <div className="toggleNavbar">
+          <TbGridDots className="icon" onClick={toggleNav} />
+        </div>
+      </header>
+    </section>
+  );
+};
+
+export default Navbar;
